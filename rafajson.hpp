3238474bbab6fc42b6e6b6bfa77feb144f77d90e
fsZhuangB <617670307@qq.com>
2019-03-07 21:26:28 +0800

    nice

--- rafajson.hpp
+++ rafajson.hpp
@@ -3,14 +3,34 @@
 #include <string>
 
 // enumerate six kind of json type
-enum class json_type { JSON_NULL, JSON_FALSE, JSON_TRUE, JSON_NUMBER, JSON_STRING, JSON_ARRAY, JSON_OBJECT };
+enum class json_type { 
+                       JSON_NULL,
+                       JSON_FALSE,
+                       JSON_TRUE,
+                       JSON_NUMBER,
+                       JSON_STRING,
+                       JSON_ARRAY,
+                       JSON_OBJECT 
+                    };
+
 
 // define json's data structure
 class json_value {
     json_type type;
 };
 
+// the return value
+enum {
+    JSON_PARSE_OK = 0,
+    JSON_PARSE_EXPECT_VALUE,
+    JSON_PARSE_INVALID_VALUE,
+    JSON_PARSE_ROOT_NOT_SINGULAR
+};
+
 /* this function parse the json*/
-int json_parse(json_value* v, const std::string json);
+int json_parse(json_value* value, const std::string json);
+
+/* this function get the type of result */
+json_type json_get_type(const json_value* value);
 
 #endif
