330b4a42dbff1534f858b648fa2235eda4f3a13d
fsZhuangB <617670307@qq.com>
2019-03-08 23:26:34 +0800

    a little

--- test.cpp
+++ test.cpp
@@ -1,24 +1,25 @@
 /* a test function for my library */
-// good
-#include "rafajson.hpp"
+
 #include <iostream>
-using namespace std;
+#include <stdlib.h>
+#include "rafajson.hpp"
+
+static int main_ret = 0;
+static int test_count = 0;
+static int test_pass = 0;
+
+#define EXPECT_EQ_BASE(equality, expect, actual, format) \
+    do {\
+        test_count++;\
+        if (equality)\
+            test_pass++;\
+        else {\
+            fprintf(stderr, "%s:%d: expect: " formart " actual: " format "\n", __FILE__, __LINE__, expect, actual);\
+            main_ret = 1;\
+        }\
+    } while(0)
+
 int main()
 {
-    enum class Enum_type {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
-    Enum_type weekDays = Enum_type::Monday;
-
-    switch (weekDays)
-    {
-        case Enum_type::Monday:
-            cout << "It's monday!" << endl;
-            break;
-    
-        default: 
-        cout << "have a nice day!" << endl;
-            break;
-    }
     return 0;
-
 }
-
