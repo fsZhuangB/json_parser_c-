e7bec568bb541d3a6175f0773bc41573c8f09c1d
fsZhuangB <617670307@qq.com>
2019-03-09 20:28:29 +0800

    go

--- test.cpp
+++ test.cpp
@@ -14,7 +14,7 @@ static int test_pass = 0;
         if (equality)\
             test_pass++;\
         else {\
-            fprintf(stderr, "%s:%d: expect: " formart " actual: " format "\n", __FILE__, __LINE__, expect, actual);\
+            fprintf(stderr, "%s:%d: expect: " format " actual: " format "\n", __FILE__, __LINE__, expect, actual);\
             main_ret = 1;\
         }\
     } while(0)
@@ -24,16 +24,19 @@ static int test_pass = 0;
 static void test_parse_null() {
     json_value v;
     v.type = json_type::JSON_TRUE;
-    EXPECT_EQ_INT(JSON_PARSE_OK, json_parse(&v, "null");
-    EXPECT_EQ_INT(JSON_NULL, json_get_type(&v);
+    EXPECT_EQ_INT(JSON_PARSE_OK, json_parse(&v, "null"));
+    EXPECT_EQ_INT(json_type::JSON_NULL, json_get_type(&v));
 }
 
-static void test_parse()
-{
+/* ... */
+
+static void test_parse() {
     test_parse_null();
+    /* ... */
 }
 
-int main()
-{
-    return 0;
+int main() {
+    test_parse();
+    std::cout << test_pass << "/" << test_count << " (" << test_pass * 100.0 / test_count << ") " << "passed" << std::endl;
+    return main_ret;
 }
