cfdcac62a981fb98855336f7c33064559d0fba2d
fsZhuangB <617670307@qq.com>
2019-03-15 22:48:36 +0800

    update new function

--- test.cpp
+++ test.cpp
@@ -1,7 +1,6 @@
 /* test file */
 
 #include <iostream>
-#include <stdlib.h>
 #include "rafajson.hpp"
 
 static int main_ret = 0;
@@ -27,11 +26,9 @@ static int test_pass = 0;
 
 static void test_parse_null() {
     json_value value;
-    value.type = json_type::JSON_TRUE;
+    value.type = json_type::JSON_NULL;
     EXPECT_EQ_INT(JSON_PARSE_OK, json_parse(&value, "null"));
     EXPECT_EQ_INT(json_type::JSON_NULL, json_get_type(&value));
-    EXPECT_EQ_INT(JSON_PARSE_ROOT_NOT_SINGULAR, json_parse(&value, "null x"));
-    EXPECT_EQ_INT(JSON_PARSE_INVALID_VALUE, json_parse(&value, "-"));
 }
 
 static void test_parse_true() {
@@ -48,12 +45,29 @@ static void test_parse_false() {
     EXPECT_EQ_INT(json_type::JSON_FALSE, json_get_type(&value));
 }
 
+static void test_invalid_value() {
+    json_value value;
+    value.type = json_type::JSON_FALSE;
+    EXPECT_EQ_INT(JSON_PARSE_INVALID_VALUE, json_parse(&value, "nul"));
+    EXPECT_EQ_INT(json_type::JSON_NULL, json_get_type(&value));
 
+    value.type = json_type::JSON_FALSE;
+    EXPECT_EQ_INT(JSON_PARSE_INVALID_VALUE, json_parse(&value, "?"));
+    EXPECT_EQ_INT(json_type::JSON_NULL, json_get_type(&value));
+}
+
+static void test_parse_expect_value() {
+    json_value value;
+    
+    value.type = json_type::JSON_FALSE;
+
+}
 
 static void test_parse() {
     test_parse_null();
     test_parse_true();
     test_parse_false();
+    test_invalid_value();
     /* ... */
 }
 
