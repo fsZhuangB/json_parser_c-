0ba8aa4f56123eb082ae3de8a502d788dac61c52
fszhuangb <617670307@qq.com>
2019-04-15 09:21:41 +0800

    ...

--- test/string_test.cpp
+++ test/string_test.cpp
@@ -8,6 +8,9 @@ int main()
     content = "json_test";
     const char * p = content.c_str();
     cout << *p << endl;
+    content += 2;
+    const char *p2 = content.c_str();
+    cout << *p2 << endl;
 
     return 0;
 }
